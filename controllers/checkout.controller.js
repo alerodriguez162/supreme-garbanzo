21eea99b65ba462137f32e2700b056e035fb8dda
alerodriguez162 <alerodriguez162@gmail.com>
2021-09-30 15:50:24 -0500

    Checkout finish & multiple fixes

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -4,19 +4,16 @@ const Cart = require("../models/Cart.model");
 
 const calculateAmount = (items) => {
   let amount = items.reduce((acc, item) => {
-    console.log(item.product.price);
-    return acc + item.product.price;
+    return acc + item.product.price * item.quantity;
   }, 0);
-  return Math.round(amount * 100);
+  return amount;
 };
 
 const createCheckout = async (req, res) => {
   try {
     const currentUser = req.session.currentUser;
 
-    const currentCart = await Cart.findOne({ user: currentUser._id }).populate(
-      "products.product"
-    );
+    const currentCart = await Cart.findOne({ user: currentUser._id }).populate("products.product");
 
     res.render("checkout/checkout", {
       products: currentCart.products,
@@ -29,22 +26,66 @@ const createCheckout = async (req, res) => {
 
 const submitCheckout = async (req, res) => {
   try {
-    console.log(req.body);
+    const { firstName, lastName, email, address, address2, state, zip, sameAddress, cardName, stripeToken } = req.body;
     // const token = req.body.stripeToken; // Using Express
 
-    // const currentUser = req.session.currentUser;
+    const currentUser = req.session.currentUser;
 
-    // const currentCart = await Cart.findOne({ user: currentUser._id }).populate("products.product");
-    // const charge = await stripe.charges.create({
-    //   amount: calculateAmount(currentCart.products),
-    //   currency: "mxn",
-    //   description: "Example charge",
-    //   source: token,
-    // });
+    const currentCart = await Cart.findOne({ user: currentUser._id }).populate("products.product");
+    const charge = await stripe.charges.create({
+      amount: calculateAmount(currentCart.products) * 100,
+      currency: "mxn",
+      description: "Example charge",
+      source: stripeToken,
+    });
+    let statusCart = 0;
+    if (charge.status === "succeeded") statusCart = 2;
+    else console.log(charge.status);
+    let newCart = await Cart.findOneAndUpdate(
+      { user: currentUser._id },
+      {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        address: address,
+        address2: address2,
+        state: state,
+        status: statusCart,
 
-    res.status(200).json({ name: "success" });
+        zip: zip,
+        sameAddress: sameAddress,
+        cardName: cardName,
+        stripeToken: stripeToken,
+      },
+      {
+        new: true,
+      }
+    ).populate("products.product");
+
+    if (statusCart === 2) {
+      let date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      let newDate = "";
+      if (month < 10) {
+        newDate = `${day}-0${month}-${year}`;
+      } else {
+        newDate = `${day}-${month}-${year}`;
+      }
+      res.render("checkout/orderComplete", {
+        date: newDate,
+        charge: charge,
+        cart: newCart,
+        products: newCart.products,
+        total: calculateAmount(currentCart.products),
+      });
+    } else {
+      res.redirect("/");
+    }
   } catch (error) {
-    res.status(200).json(error.message);
+    res.redirect("/");
   }
 };
 
--- controllers/plp.controller.js
+++ controllers/plp.controller.js
@@ -2,14 +2,14 @@ const Product = require("../models/Product.model");
 
 const getProducts = async (req, res, next) => {
   try {
-    const params = req.params;
+    const querys = req.query;
 
-    if (!params) {
+    if (!querys) {
       const products = await Product.find({});
       console.log(products);
       res.render("plp/plp", { products: products });
     } else {
-      const products = await Product.find({});
+      const products = await Product.find({ title: { $regex: querys.search || "", $options: "i" } });
       res.render("plp/plp", { products: products });
     }
   } catch (error) {
