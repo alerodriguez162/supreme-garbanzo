ddb9b6260a060a5598f6c4c72d365874cfdb4761
alerodriguez162 <alerodriguez162@gmail.com>
2021-09-30 11:08:18 -0500

    fixes

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -61,11 +61,11 @@ document.addEventListener(
       const { token, error } = await stripe.createToken(cardNumberElement);
 
       if (!error) {
-        // const hiddenInput = document.createElement("input");
-        // hiddenInput.setAttribute("type", "hidden");
-        // hiddenInput.setAttribute("name", "stripeToken");
-        // hiddenInput.setAttribute("value", token.id);
-        // form.appendChild(hiddenInput);
+        const hiddenInput = document.createElement("input");
+        hiddenInput.setAttribute("type", "hidden");
+        hiddenInput.setAttribute("name", "stripeToken");
+        hiddenInput.setAttribute("value", token.id);
+        form.appendChild(hiddenInput);
         form.submit();
       }
     });
